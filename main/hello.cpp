d8c6852f9d763a7b071b1887a77e3540f0a38f71
True-Fox <pes1202100682@pesu.pes.edu>
2024-03-05 15:14:14 +0530

    commit-5

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Changes made\n";
+  cout << "Webhook successfully enabled\nPES1UG21CS659\n";
   return 0;
 }
